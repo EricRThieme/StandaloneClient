794dda9c57172282ff5ec24365c75fae72cfdbfc
Eric Thieme <eric.rainer.thieme@gmail.com>
2026-01-19 22:09:47 +0100

    fix: remove unnecessary safe cast for scalar nodes in YamlConfigService

--- src/main/kotlin/org/stt/config/YamlConfigService.kt
+++ src/main/kotlin/org/stt/config/YamlConfigService.kt
@@ -101,7 +101,7 @@ constructor(@param:Named("homePath") val homePath: String) : Service, ConfigServ
 
             yamlConstructors[TAG_DURATION] = object : AbstractConstruct() {
                 override fun construct(node: Node): Any {
-                    val durationString = constructScalar(node as ScalarNode) as? String
+                    val durationString = constructScalar(node as ScalarNode)
                         ?: throw ClassCastException("Expected scalar string for !duration, got: ${node.nodeId}")
                     return Duration.between(LocalTime.MIDNIGHT, LocalTime.parse(durationString,
                             DateTimes.DATE_TIME_FORMATTER_HH_MM_SS))
@@ -109,15 +109,14 @@ constructor(@param:Named("homePath") val homePath: String) : Service, ConfigServ
             }
             yamlConstructors[TAG_PATH] = object : AbstractConstruct() {
                 override fun construct(node: Node): Any {
-                    val path = constructScalar(node as ScalarNode) as? String
+                    val path = constructScalar(node as ScalarNode)
                         ?: throw ClassCastException("Expected scalar string for !path, got: ${node.nodeId}")
                     return PathSetting(path)
                 }
             }
             yamlConstructors[TAG_ENCRYPTED] = object : AbstractConstruct() {
                 override fun construct(node: Node): Any? {
-                    val base64EncryptedPassword = constructScalar(node as ScalarNode) as? String
-                        ?: return null
+                    val base64EncryptedPassword = constructScalar(node as ScalarNode) ?: return null
                     try {
                         return PasswordSetting.fromEncryptedPassword(Base64.getDecoder().decode(base64EncryptedPassword))
                     } catch (e: Exception) {
